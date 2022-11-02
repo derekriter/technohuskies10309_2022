37e52a4ff6c3fe95e1c1885f737e99a7252df61f
RussellStuff <russellkalcec@gmail.com>
2022-11-02 18:37:39 -0400

    Code for the left side of the field

---
+++ Team10309/src/main/java/org/firstinspires/ftc/team10309/autoOp/RussellAutoOpBl.java
@@ -0,0 +1,41 @@
+    package org.firstinspires.ftc.team10309.autoOp;
+
+    import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+    import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+    import org.firstinspires.ftc.team10309.API.ClawController;
+    import org.firstinspires.ftc.team10309.API.Robot;
+
+    @Autonomous(name="RussellAutoOpBl", group="Examples")
+    public class RussellAutoOpBl extends LinearOpMode {
+
+        private Robot robot;
+        private ClawController clawController;
+
+        @Override
+        public void runOpMode() throws InterruptedException {
+            //init
+            this.robot = new Robot(this, true);
+
+            waitForStart();
+
+                Robot robot = new Robot(this, true);
+                robot.initDetect();
+                telemetry.addLine("Press Start");
+                telemetry.update();
+                waitForStart();
+                telemetry.addLine(robot.scanSleeve().name());
+                telemetry.update();
+                Thread.sleep(1000);
+            robot.turn(90,.7f,1f);
+            robot.strafe(-21,.7f);
+            robot.drive(-32,.7f);
+            robot .drive(-2,.7f);
+             clawController.setClawState(ClawController.ClawState.OPEN);
+             robot.strafe(-24,.7f);
+            clawController.setLiftPosition(ClawController.LiftPosition.valueOf());
+            robot.strafe(-2,.7f);
+
+
+        }
+    }
