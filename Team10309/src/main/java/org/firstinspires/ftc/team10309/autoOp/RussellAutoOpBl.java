65522065e290ebb274dca9334a856fa58bc9c2f2
RussellStuff <russellkalcec@gmail.com>
2022-11-02 19:13:33 -0400

    Basic move to code

--- Team10309/src/main/java/org/firstinspires/ftc/team10309/autoOp/RussellAutoOpBl.java
+++ Team10309/src/main/java/org/firstinspires/ftc/team10309/autoOp/RussellAutoOpBl.java
@@ -19,7 +19,7 @@
 
             waitForStart();
 
-                Robot robot = new Robot(this, true);
+                robot = new Robot(this, true);
                 robot.initDetect();
                 telemetry.addLine("Press Start");
                 telemetry.update();
@@ -27,6 +27,7 @@
                 telemetry.addLine(robot.scanSleeve().name());
                 telemetry.update();
                 Thread.sleep(1000);
+                robot.strafe(4,.7f);
             robot.turn(90,.7f,1f);
             robot.strafe(-21,.7f);
             robot.drive(-32,.7f);
