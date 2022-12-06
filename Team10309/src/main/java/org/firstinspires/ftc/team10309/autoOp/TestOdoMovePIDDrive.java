927e3c2e771a2c26df0f4cc37a32475385bffb8a
TechnoTurtle7 <technoturtle7d@gmail.com>
2022-12-06 18:59:50 -0500

    Moving experimental odo moving between computers

--- Team10309/src/main/java/org/firstinspires/ftc/team10309/autoOp/TestOdoMovePIDDrive.java
+++ Team10309/src/main/java/org/firstinspires/ftc/team10309/autoOp/TestOdoMovePIDDrive.java
@@ -2,13 +2,11 @@ package org.firstinspires.ftc.team10309.autoOp;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
 
 import org.firstinspires.ftc.team10309.API.Robot;
 
-@Autonomous(name="Test Odo Move1")
-public class TestOdoMovePID extends LinearOpMode {
+@Autonomous(name="Test Odo Move Drive")
+public class TestOdoMovePIDDrive extends LinearOpMode {
 
     private Robot robot;
 
@@ -18,8 +16,9 @@ public class TestOdoMovePID extends LinearOpMode {
         this.robot = new Robot(this, true);
 
         telemetry.addLine("Init Completed");
+        telemetry.update();
         waitForStart();
 
-        robot.driveOdo(120, 0.2f);
+        robot.driveOdo(36, 0.6f);
     }
 }
---
+++ Team10309/src/main/java/org/firstinspires/ftc/team10309/autoOp/TestOdoMovePIDStrafe.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.team10309.autoOp;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.team10309.API.Robot;
+
+@Autonomous(name="Test Odo Move Strafe")
+public class TestOdoMovePIDStrafe extends LinearOpMode {
+
+    private Robot robot;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        //init
+        this.robot = new Robot(this, true);
+
+        telemetry.addLine("Init Completed");
+        telemetry.update();
+        waitForStart();
+
+        robot.strafeOdo(36, 0.6f);
+    }
+}
