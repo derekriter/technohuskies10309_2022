7aab44a35945701ff73d5b2030c49ee48244069f
Ben100000000000 <benjvans27@midlandps.org>
2022-12-14 20:21:29 -0500

    Made BLUEBLUE and REDRED what they are supposed to be

--- Team10309/src/main/java/org/firstinspires/ftc/team10309/autoOp/BenREDREDAutoOp.java
+++ Team10309/src/main/java/org/firstinspires/ftc/team10309/autoOp/BenREDREDAutoOp.java
@@ -1,14 +1,14 @@
 package org.firstinspires.ftc.team10309.autoOp;
 
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+        import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+        import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-import org.firstinspires.ftc.team10309.API.ManipulatorController;
-import org.firstinspires.ftc.team10309.API.Robot;
-import org.firstinspires.ftc.team10309.API.SleeveDetect;
+        import org.firstinspires.ftc.team10309.API.ManipulatorController;
+        import org.firstinspires.ftc.team10309.API.Robot;
+        import org.firstinspires.ftc.team10309.API.SleeveDetect;
 
 // Algin with a jenga block 1 jenga block from the right side edge of mat.
-@Autonomous(name = "BenBlueBlue")
+@Autonomous(name = "BenRedRed")
 public class BenREDREDAutoOp extends LinearOpMode {
 
     private Robot robot;
@@ -43,9 +43,8 @@ public class BenREDREDAutoOp extends LinearOpMode {
 
         //go to low junction
         manipulatorController.setLiftPosition(ManipulatorController.LiftPosition.LOW, false);
-        this.robot.drive(2f, 0.5f);
         this.robot.strafeTiles(0.7f, 0.5f, -0.5f);
-        this.robot.drive(4f, 0.5f);
+        this.robot.drive(-1.5f, 0.5f);
         Thread.sleep(200);
 
         //drop cone on low junction
@@ -54,23 +53,22 @@ public class BenREDREDAutoOp extends LinearOpMode {
         manipulatorController.setLiftPosition(this.robot.getHardware().getLift().getCurrentPosition() - 500);
 
         //go to cone stack
-        this.robot.drive(-3.5f, 0.5f);
+        this.robot.drive(1.5f, 0.5f);
         this.robot.strafeTiles(1.5f, 0.5f, -3.5f);
-        manipulatorController.setArmPosition(ManipulatorController.ArmRotation.BACK);
+        manipulatorController.setArmPosition(ManipulatorController.ArmRotation.FRONT);
 
         //pick up cone
         manipulatorController.setLiftPosition(-1256);
-        this.robot.driveTiles(-1f, 0.5f);
+        this.robot.driveTiles(1f, 0.5f);
         manipulatorController.setClaw(ManipulatorController.ClawPosition.CLOSED);
         manipulatorController.setLiftPosition(ManipulatorController.LiftPosition.LOW);
         manipulatorController.setLiftPosition(ManipulatorController.LiftPosition.HIGH, false);
 
         //go to tall junction
-        this.robot.drive(6, 0.2f);
+        this.robot.drive(-6, 0.2f);
         this.robot.turn(45f, .2 );
         this.robot.turn(45f, .2);
-        this.robot.strafeTiles(-1.5f, 0.5f);
-        manipulatorController.setArmPosition(armPosLeft);
+        this.robot.strafeTiles(1.5f, 0.5f);
         this.robot.drive(3f, 0.5f);
 
         // drop onto high junction
@@ -79,16 +77,16 @@ public class BenREDREDAutoOp extends LinearOpMode {
 
         // detection stuff
         if (state == SleeveDetect.SignalState.RED_SQUARE) {
-            this.robot.strafeTiles(-0.5f, 0.5f);
+            this.robot.strafeTiles(-1.5f, 0.5f);
             manipulatorController.setLiftPosition(ManipulatorController.LiftPosition.GROUND);
         } else if (state == SleeveDetect.SignalState.GREEN_CIRCLE) {
-            this.robot.strafeTiles(0.5f, 0.5f);
+            this.robot.strafeTiles(-0.5f, 0.5f);
             manipulatorController.setLiftPosition(ManipulatorController.LiftPosition.GROUND);
         } else if (state == SleeveDetect.SignalState.BLUE_TRIANGLE) {
-            this.robot.strafeTiles(1.5f, 0.5f);
+            this.robot.strafeTiles(0.5f, 0.5f);
             manipulatorController.setLiftPosition(ManipulatorController.LiftPosition.GROUND);
         } else {
-            this.robot.strafeTiles(0.5f, 0.5f);
+            this.robot.strafeTiles(-0.5f, 0.5f);
             manipulatorController.setLiftPosition(ManipulatorController.LiftPosition.GROUND);
 
         }
